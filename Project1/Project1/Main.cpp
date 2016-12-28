1825329c7d2784e3de147ca631f9aa5a0ae4910d
chirilavasiletiberiu <chirila.vasiletiberiu@gmail.com>
2016-12-28 17:22:03 +0200

    1.1
    - adding SDL image

--- Project1/Project1/Main.cpp
+++ Project1/Project1/Main.cpp
@@ -1,12 +1,16 @@
 
 #include "Main.h"
 
-#include "SDL.h"
+using namespace std;
 
 int main(int argc, char** argv)
 {
+	std::cout << "test";
+	printf("testele mele !");
+
 	SDL_Init(SDL_INIT_EVERYTHING);
 
 	SDL_Quit();
+
 	return 0;
 }
--- Project1/Project1/Main.h
+++ Project1/Project1/Main.h
@@ -1,1 +1,7 @@
 #pragma once
+
+#include "iostream"
+#include <stdio.h>
+
+#include "SDL.h"
+#include "SDL_image.h"
