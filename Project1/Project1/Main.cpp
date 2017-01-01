cc99b00c17d91a8bc8500f6ec1579f66fa3175ac
chirilavasiletiberiu <chirila.vasiletiberiu@gmail.com>
2017-01-01 18:45:24 +0200

    - initial game
    - creating the Field object
    - a lot of time spent to load a simple image
    - creating the game window

--- Project1/Project1/Main.cpp
+++ Project1/Project1/Main.cpp
@@ -1,16 +1,63 @@
-
 #include "Main.h"
 
 using namespace std;
 
 int main(int argc, char** argv)
 {
-	std::cout << "test";
-	printf("testele mele !");
-
 	SDL_Init(SDL_INIT_EVERYTHING);
+
+	SDL_Window *window = SDL_CreateWindow("Minesweeper", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, fieldsX * 30, fieldsY * 30, 0);
+	SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+
+	Field fields[fieldsX * fieldsY];
+
+	SDL_Texture *emptyImage = IMG_LoadTexture(renderer, "../images/empty.bmp");
+
+	initGame(renderer, emptyImage, fields);
+
+	SDL_Event event;
+	bool running = true;
+
+	while (running == true)
+	{
+		if (SDL_PollEvent(&event))
+		{
+
+		}
+	}
+
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyWindow(window);
 
 	SDL_Quit();
 
 	return 0;
+}
+
+void initGame(SDL_Renderer *renderer, SDL_Texture *image, Field fields[])
+{
+	SDL_RenderClear(renderer);
+
+	for (int i = 0; i < fieldsX; i++)
+	{
+		for (int j = 0; j < fieldsY; j++)
+		{
+			fields[i + j*fieldsX].state = Hidden;
+			initFieldOffSet(renderer, image, i, j);
+		}
+	}
+
+	SDL_RenderPresent(renderer);
+}
+
+void initFieldOffSet(SDL_Renderer *renderer, SDL_Texture *image, int x, int y)
+{
+	SDL_Rect destinationRect;
+
+	destinationRect.x = x*fieldsX;
+	destinationRect.y = y*fieldsX;
+	destinationRect.w = fieldsX;
+	destinationRect.h = fieldsX;
+
+	SDL_RenderCopy(renderer, image, NULL, &destinationRect);
 }
--- Project1/Project1/Main.h
+++ Project1/Project1/Main.h
@@ -2,6 +2,35 @@
 
 #include "iostream"
 #include <stdio.h>
+#include <time.h>
 
 #include "SDL.h"
 #include "SDL_image.h"
+
+#define fieldsX 30
+#define fieldsY 30
+
+enum FieldState 
+{
+	Empty = 0,
+	Bomb1 = 1,
+	Bomb2 = 2,
+	Bomb3 = 3,
+	Bomb4 = 4,
+	Bomb5 = 5,
+	Bomb6 = 6,
+	Bomb7 = 7,
+	Bomb8 = 8,
+	Bomb = 9,
+	Flag = 10,
+	Hidden = 11
+};
+
+typedef struct 
+{
+	int initialState;
+	int state;
+} Field;
+
+void initGame(SDL_Renderer *renderer, SDL_Texture *image, Field fields[]);
+void initFieldOffSet(SDL_Renderer *renderer, SDL_Texture *image, int x, int y);
